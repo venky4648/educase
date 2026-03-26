c9b6a1b257cf4d32f244792fa88d2848e077a764
venkatesh kota <kotav0835@gmail.com>
2026-03-26 13:27:09 +0530

    commit

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,10 +3,16 @@ import dotenv from 'dotenv'
 
 dotenv.config()
 
-const sequelize = new Sequelize(process.env.DB_NAME,process.env.DB_USER, process.env.DB_PASSWORD, {
-  host: "localhost",
-  dialect: "mysql",
-});
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    dialect: "mysql",
+  }
+);
 
 sequelize
   .authenticate()
